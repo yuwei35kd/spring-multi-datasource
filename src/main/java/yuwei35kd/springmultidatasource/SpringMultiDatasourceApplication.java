093fc869622c6f8a4d22339553b864a9f8ebedd4
yuwei35kd <yuwei35kd@163.com>
2017-11-01 16:38:25 +0800

    增加注意事项

--- src/main/java/yuwei35kd/springmultidatasource/SpringMultiDatasourceApplication.java
+++ src/main/java/yuwei35kd/springmultidatasource/SpringMultiDatasourceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -23,6 +24,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * http://www.cnblogs.com/digdeep/p/4512368.html
  * 
  * https://stackoverflow.com/questions/14068525/javaconfig-replacing-aopadvisor-and-txadvice
+ * 
+ * 注意：
+ * 当是不同类型的数据源切换时需要注意的是，需要将在开启事务之前切换数据源。
+ * 比方两个数据源：一个是mysql,一个是oracle
+ * 此时如果在service层开启事务，而数据源切换在dao层，这时就会报错。
+ * 最好是开始事务和切换数据源都在service上面。而DataSourceAspect上面的@Order(0)决定了切换数据源会在开始事务之前。
+ * 
+ * 
  */
 @SpringBootApplication
 @EnableAspectJAutoProxy
